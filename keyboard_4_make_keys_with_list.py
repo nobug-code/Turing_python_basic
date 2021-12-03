c0ef34d83d35c5b1d1095f7ee81ea1483508ef63
Namgil Kim <tompx@naver.com>
2021-12-03 17:51:35 +0900

    Add files via upload

---
+++ keyboard_4_make_keys_with_list.py
@@ -0,0 +1,74 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+# 카메라를 VideoCapture 객체로 가져온다.
+cap = cv2.VideoCapture(0)
+
+# Minimum Detection Confidence Threshold
+detector = HandDetector(detectionCon=0.8)
+
+keys = [["Q", "W", "E"]]
+
+colorR = (255, 0, 255)
+
+class Button():
+
+    def __init__(self, pos, text, size=[100, 100]):
+        self.pos = pos
+        self.text = text
+        self.size = size
+
+    def draw_box(self, img):
+        x, y = self.pos
+        w, h = self.size
+
+        # 그냥 상자 그리기
+        cv2.rectangle(img, self.pos, (x + w, y + h), colorR, cv2.FILLED)
+        # 글 그려서 넣깅 , 4번째는 font scale
+        cv2.putText(img, self.text, (x + 10, y + 10),
+        cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+        return img
+
+
+buttonList = []
+
+for x in range(0, 5):
+
+    buttonList.append(Button([100 + x * 150, 100 ], "Q"))
+
+# list로 만들어줘
+
+
+while True:
+    # 계속해서 이미지를 불러와서 영상을 보여준다.
+    success, img = cap.read()
+    # Find the hand and its landmarks
+    # 이미지 좌우 반전
+    img = cv2.flip(img, 1)
+    hands, img = detector.findHands(img)  # with draw
+
+    # 그림 거려
+    # img = mybuton.draw_box(img)
+    # img = mybuton1.draw_box(img)
+    # img = mybuton2.draw_box(img)
+    for button in buttonList:
+        img = button.draw_box(img)
+
+
+    if hands:
+        # Hand 1
+        hand1 = hands[0]
+        lmList1 = hand1["lmList"]  # List of 21 Landmark points
+        bbox1 = hand1["bbox"]  # Bounding box info x,y,w,h
+        centerPoint1 = hand1['center']  # center of the hand cx,cy
+        handType1 = hand1["type"]  # Handtype Left or Right
+        fingers1 = detector.fingersUp(hand1)
+
+        l, _, _ = detector.findDistance(lmList1[8], lmList1[12], img)
+
+
+
+    # Display
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+cap.release()
+cv2.destroyAllWindows()
---
+++ keyboard_8_with_change_length.py
@@ -0,0 +1,92 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+# 카메라를 VideoCapture 객체로 가져온다.
+cap = cv2.VideoCapture(0)
+
+# Minimum Detection Confidence Threshold
+detector = HandDetector(detectionCon=0.8)
+
+keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+        ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
+        ["Z", "X", "C", "V", "B", "N", "M", ",", "." ,"/"]]
+
+
+colorR = (255, 0, 255)
+
+class Button():
+
+    def __init__(self, pos, text, size=[85, 85]):
+        self.pos = pos
+        self.text = text
+        self.size = size
+
+finalText =[]
+
+# list로 만들어줘
+buttonList = []
+for out_num, key in enumerate(keys):
+    for num, key_elm in enumerate(key):
+        buttonList.append(Button([100 * num + 50, 100 * out_num + 50], key_elm))
+
+def drawALL(img, buttonList):
+
+    for button in buttonList:
+
+        x, y = button.pos
+        w, h = button.size
+        # 그냥 상자 그리기
+        cv2.rectangle(img, button.pos, (x + w, y + h), colorR, cv2.FILLED)
+        # 글 그려서 넣깅 , 4번째는 font scale, 칼러 다음에는 두께
+        cv2.putText(img, button.text, (x + 20, y + 65),
+                    cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+
+    return img
+while True:
+    # 계속해서 이미지를 불러와서 영상을 보여준다.
+    success, img = cap.read()
+    # Find the hand and its landmarks
+    # 이미지 좌우 반전
+    img = cv2.flip(img, 1)
+    hands, img = detector.findHands(img)  # with draw
+    img = drawALL(img, buttonList)
+    if hands:
+        # Hand 1
+        hand1 = hands[0]
+        lmList1 = hand1["lmList"]  # List of 21 Landmark points
+        bbox1 = hand1["bbox"]  # Bounding box info x,y,w,h
+        centerPoint1 = hand1['center']  # center of the hand cx,cy
+        handType1 = hand1["type"]  # Handtype Left or Right
+        fingers1 = detector.fingersUp(hand1)
+
+
+        for button in buttonList:
+            x, y = button.pos
+            w, h = button.size
+            if x < lmList1[8][0] < x + w and y < lmList1[8][1] < y + h:
+                cv2.rectangle(img, button.pos, (x + w, y + h), (175, 0, 175), cv2.FILLED)
+                # 글 그려서 넣깅 , 4번째는 font scale, 칼러 다음에는 두께
+                cv2.putText(img, button.text, (x + 20, y + 65),
+                            cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+
+                l, _, _ = detector.findDistance(lmList1[8], lmList1[12], img)
+
+                if l < 50:
+                    cv2.rectangle(img, button.pos, (x + w, y + h), (0, 255, 0), cv2.FILLED)
+                    # 글 그려서 넣깅 , 4번째는 font scale, 칼러 다음에는 두께
+                    cv2.putText(img, button.text, (x + 20, y + 65),
+                                cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+
+                    finalText += button.text
+
+                    # sleep(0.15)
+
+    cv2.rectangle(img, (50, 350), (700, 450), (175, 0, 175), cv2.FILLED)
+    # 글 그려서 넣깅 , 4번째는 font scale, 칼러 다음에는 두께
+    cv2.putText(img, finalText , (60, 430),
+                            cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+
+    # Display
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+cap.release()
+cv2.destroyAllWindows()
